246b987d34d121a7d9af16d74e59bb0d1928434e
Pham Minh Dao <daopm.dean@gmail.com>
2024-06-30 00:13:31 +0700

    building response

--- common/wrapper.go
+++ common/wrapper.go
@@ -1,36 +1,72 @@
 package common
 
 func UnauthenticatedRes() *Response {
-	return &Response{
-		Status: ResponseStatus.Unauthorized,
-		Errors: []*ErrRes{
-			{
-				ErrCode: "UNAUTHENTICATED",
-				ErrMsg:  "Unauthenticated",
-			},
-		},
-	}
+	return BuildUnauthorizedRes("UNAUTHENTICATED", "Unauthenticated")
 }
 
 func UnauthorizedRes() *Response {
+	return BuildUnauthorizedRes("UNAUTHORIZED", "Unauthorized")
+}
+
+func BuildUnauthorizedRes(errCode, errMsg string) *Response {
 	return &Response{
 		Status: ResponseStatus.Unauthorized,
 		Errors: []*ErrRes{
 			{
-				ErrCode: "UNAUTHORIZED",
-				ErrMsg:  "Unauthorized",
+				ErrCode: errCode,
+				ErrMsg:  errMsg,
 			},
 		},
 	}
 }
 
 func InvalidRes(msg string) *Response {
+	return BuildInvalidRes("INVALID", msg)
+}
+
+func BuildInvalidRes(errCode, errMsg string) *Response {
 	return &Response{
 		Status: ResponseStatus.Invalid,
 		Errors: []*ErrRes{
 			{
-				ErrCode: "INVALID",
-				ErrMsg:  msg,
+				ErrCode: errCode,
+				ErrMsg:  errMsg,
+			},
+		},
+	}
+}
+
+func BuildErrorRes(errCode, errMsg string) *Response {
+	return &Response{
+		Status: ResponseStatus.Error,
+		Errors: []*ErrRes{
+			{
+				ErrCode: errCode,
+				ErrMsg:  errMsg,
+			},
+		},
+	}
+}
+
+func BuildErrorRes(errCode, errMsg string) *Response {
+	return &Response{
+		Status: ResponseStatus.NotFound,
+		Errors: []*ErrRes{
+			{
+				ErrCode: errCode,
+				ErrMsg:  errMsg,
+			},
+		},
+	}
+}
+
+func BuildRes(status ResponseStatusValue, errCode, errMsg string) *Response {
+	return &Response{
+		Status: status,
+		Errors: []*ErrRes{
+			{
+				ErrCode: errCode,
+				ErrMsg:  errMsg,
 			},
 		},
 	}
