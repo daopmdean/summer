626a42c04eca5ecfc79b51e02ac40eefe974c047
DaoPM Dean <daopm.dean@gmail.com>
2024-08-29 17:05:53 +0700

    update summer claim

--- auth/summer_claim.go
+++ auth/summer_claim.go
@@ -4,6 +4,7 @@ import "github.com/golang-jwt/jwt/v5"
 
 type SummerClaim struct {
 	jwt.RegisteredClaims
+	UserId   int64  `json:"userId"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Phone    string `json:"phone"`
